a0ff7bafb94dce13f397863b83ee775b51f2db84
jrasmusbm <JRasmusBm@gmail.com>
2019-05-01 14:06:17 +0200

    Get the first page of relocate.me

--- scrape.py
+++ scrape.py
@@ -1,26 +1,63 @@
 from bs4 import BeautifulSoup
 from pprint import pprint
-import requests
+from dataclasses import dataclass
+import grequests
+import asyncio
 
-sites = {
-    "relocate": {
-        "url": "https://relocate.me/search/software/the-netherlands?time=any",
-        "href": "/the-netherlands/",
-    }
+relocate = {
+    "url": "https://relocate.me",
+    "search": "/search/software/the-netherlands?time=any",
+    "href": "/the-netherlands/",
 }
 
 
-def scrape(site):
-    base_url = site.get("url", "")
-    response = requests.get(base_url, timeout=5)
-    content = BeautifulSoup(response.content, "html.parser")
+@dataclass
+class Offer:
+    title: str
+    company: str
+
+    def __str__(self):
+        return f"""
+            Title: {self.title}
+            Company: {self.company}
+            """
+
+
+async def job_links(site):
+    base_url = site.get("url", "") + site.get("search", "")
+    response = grequests.map([grequests.get(base_url, timeout=5)])[0]
+    soup = BeautifulSoup(response.content, "html.parser")
     links = [
-        link.get("href")
-        for link in content.find_all("a")
+        site.get("url", "") + link.get("href")
+        for link in soup.find_all("a")
         if site.get("href", "") in link.get("href")
     ]
-    pprint(links)
+    return links
 
 
-for site in sites:
-    scrape(sites[site])
+async def extract_title(soup):
+    return "".join(soup.select(".job-info__heading h1")[0].contents).strip()
+
+
+async def extract_company(soup):
+    company_tag = soup.select(".job-info__country")[0]
+    company_tag.img.extract()
+    return "".join(company_tag.contents).strip()
+
+
+async def extract_job(response):
+    soup = BeautifulSoup(response.content, "html.parser")
+    offer = Offer(title=await extract_title(soup), company=await extract_company(soup))
+    print(offer)
+
+
+async def jobs():
+    relocate_me_links = await job_links(relocate)
+    pprint(relocate_me_links)
+    return [
+        await extract_job(job)
+        for job in grequests.map(grequests.get(link) for link in relocate_me_links)
+    ]
+
+
+print(asyncio.run(jobs()))
