f993ca48cb16dd535d1b793552d66d7d9ce740f8
jrasmusbm <JRasmusBm@gmail.com>
2019-05-10 14:02:18 +0200

    Connect to back-end

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,12 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import OrderOverview from "../components/orderoverview"
+import OfferOverview from "../components/offeroverview"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Job Market Explorer" keywords={[`job`, `market`, `offers`]} />
-    <OrderOverview />
+    <OfferOverview />
   </Layout>
 )
 
